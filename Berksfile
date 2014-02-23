d60121e16351606912268194483a5d9226490aea
Ryutaro YOSHIBA <ryuzee@gmail.com>
2014-02-23 18:19:59 +0900

    bumped version

--- Berksfile
+++ Berksfile
@@ -2,4 +2,4 @@ site :opscode
 
 metadata
 
-cookbook 'nginx', git:"https://github.com/ryuzee-cookbooks/nginx.git"
+cookbook 'nginx', git:"https://github.com/ryuzee-cookbooks/nginx.git", tag:"v0.1.1"
--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'MIT'
 description      'Installs/Configures php-env'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'yum', ">= 3.0"
+depends          'yum', "~> 3.0.4"
 depends          'yum-epel'
-depends          'nginx'
+depends          'nginx', "~> 0.1.1"
